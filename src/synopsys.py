d758aa50daae91adfc046ffc0f77707561fe2db2
James Laderoute <jimladeroute@gmail.com>
2023-12-10 17:21:25 -0500

    Part2 have a circle drawn and a polygon drawn and moving

--- src/synopsys.py
+++ src/synopsys.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pygame
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 append_path = cwd + "/../lib"
@@ -9,9 +10,43 @@ import jjl_3d as _3d
 
 
 def main(argv):
-    p = _3d.Point2D()
-    p.print()
+    # Initialize pygame
+    pygame.init()
+    pygame.display.set_caption("Synopsys")
+    surface = pygame.display.set_mode((640, 480))
+    color = (55, 255, 255)
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit(0)
+    
+        surface.fill((0, 0, 0))
+    
+        pygame.draw.circle(surface, color, (320, 240), 75)
+    
+        try:
+            poly1
+        except NameError:
+            poly1 = list(((320, 240), (320, 340), (420, 340), (420, 240)))
+        else:
+            poly1[0] = (poly1[0][0] + 1, poly1[0][1] + 1)
+            poly1[1] = (poly1[1][0] + 1, poly1[1][1] + 1)
+            poly1[2] = (poly1[2][0] + 1, poly1[2][1] + 1)
+            poly1[3] = (poly1[3][0] + 1, poly1[3][1] + 1)
+
+
+                      
+        pygame.draw.polygon(surface, color, poly1)
+        
+        pygame.display.flip()
+        pygame.display.update()
+        pygame.time.delay(100)
+
+    pygame.quit()
     exit(0)
 
+    
 if __name__ == "__main__":
     main( sys.argv )
