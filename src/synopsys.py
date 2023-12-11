97a2dff743277f506bfafc1f9f4f2244e68607ae
James Laderoute <jimladeroute@gmail.com>
2023-12-10 19:32:17 -0500

    Added variable speration - fixed the letter s so it's centered like the others

--- src/synopsys.py
+++ src/synopsys.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pygame
+import math
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 append_path = cwd + "/../lib"
@@ -13,19 +14,23 @@ def main(argv):
     surface = pygame.display.set_mode((640, 480))
     color = (55, 255, 255)
     line_color = (255, 255, 255)
-    
+    clock =  pygame.time.Clock()
 
     letter_s = list (
         (
-            (-0.2, 0.5),
-            (0, 1),
-            (1, 1),
-            (1, 0),
-            (0, 0),
-            (0, -1),
-            (1, -1),
+            (-0.6, 0.5),
+            (-0.4, 1),
+            (0.6, 1),
+            (0.6, 0),
+            (-0.4, 0),
+            (-0.4, -1),
+            (0.6, -1),
         )
     )
+#    for i in range(len(letter_s)): letter_s[i] = ( letter_s[i][0] -0.40, letter_s[i][1] )
+#    letter_s = [(p[0]-.4, p[1]) for p in letter_s]
+#    print(letter_s)
+
     letter_y = [
         (-1, -1),
         (0, 0),
@@ -61,7 +66,8 @@ def main(argv):
 
     xscale = 20
     yscale = 40
-    multx = 1.0
+    theta = 0
+    seperation = 20
 
     while True:
         for event in pygame.event.get():
@@ -71,15 +77,18 @@ def main(argv):
     
         surface.fill((0, 0, 0))
     
+        xscale = math.sin(theta) * 20.0
+        yscale = math.cos(theta) * 20.0
+
         #pygame.draw.circle(surface, color, (320, 240), 75, width=1)
-        s1 = adjust_letter(letter_s, xscale, yscale,  20,    200)
-        y1 = adjust_letter(letter_y, xscale, yscale,  20*4,  200)
-        n1 = adjust_letter(letter_n, xscale, yscale,  20*7,  200)
-        o1 = adjust_letter(letter_o, xscale, yscale,  20*10, 200)
-        p1 = adjust_letter(letter_p, xscale, yscale,  20*13, 200)
-        s2 = adjust_letter(letter_s, xscale, yscale,  20*16, 200)
-        y2 = adjust_letter(letter_y, xscale, yscale,  20*19, 200)
-        s3 = adjust_letter(letter_s, xscale, yscale,  20*22, 200)
+        s1 = adjust_letter(letter_s, xscale, yscale,  seperation*1,  200)
+        y1 = adjust_letter(letter_y, xscale, yscale,  seperation*4,  200)
+        n1 = adjust_letter(letter_n, xscale, yscale,  seperation*7,  200)
+        o1 = adjust_letter(letter_o, xscale, yscale,  seperation*10, 200)
+        p1 = adjust_letter(letter_p, xscale, yscale,  seperation*13, 200)
+        s2 = adjust_letter(letter_s, xscale, yscale,  seperation*16, 200)
+        y2 = adjust_letter(letter_y, xscale, yscale,  seperation*19, 200)
+        s3 = adjust_letter(letter_s, xscale, yscale,  seperation*22, 200)
 
    
         pygame.draw.lines(surface, color, False, s1, width=1)                      
@@ -94,13 +103,17 @@ def main(argv):
 
         pygame.display.flip()
         pygame.display.update()
-        pygame.time.delay(100)
 
-        xscale += multx
-        if xscale > 46:
-            multx = -1*multx
-        elif xscale < 10:
-            multx = -1 * multx
+        theta += 0.1
+        #yscale = cos(theta)
+
+#        xscale += multx
+#        if xscale > 46:
+#            multx = -1*multx
+#        elif xscale < 10:
+#            multx = -1 * multx
+
+        clock.tick(30)
 
     pygame.quit()
     exit(0)
